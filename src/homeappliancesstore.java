e6283dcfba8c7f9de16636f9b554522c22fd0265
OYRANIA NTRIZI <ds18083@go.uop.gr>
2020-11-10 10:55:34 +0200

    Initial commit

---
+++ src/homeappliancesstore.java
@@ -0,0 +1,24 @@
+public class homeappliancesstore {
+        static String name;
+        static String address;
+        static int employees;
+
+    public static void main(String[] args) {
+        if (name == null)
+            System.out.println("error1");
+        else
+            System.out.println(name);
+        if (address == null)
+            System.out.println("error2");
+        else
+            System.out.println(address);
+        if (employees == 0)
+            System.out.println("error3");
+        else
+            System.out.println(employees);
+    }
+}
+
+
+
+
